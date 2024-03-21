1fe272bf0fff2716584d250e52e56413638ed7f9
Artem <artem_lysak07@mail.ru>
2024-03-21 23:18:13 +0300

    Start

---
+++ src/main.py
@@ -0,0 +1,14 @@
+import uvicorn
+from fastapi import FastAPI
+
+
+app = FastAPI(title='StockAnalysis')
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host='0.0.0.0',
+        port=8000,
+        reload=True
+    )
